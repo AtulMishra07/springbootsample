fde3e85fde80c8eca0ce21d25afd4698d3100789
Atul Mishra <Atul Mishra@DESKTOP-1IBU0RJ>
2019-02-26 23:10:53 +0530

    udpate sample

---
+++ springbootsample/springbootsample/src/main/java/springbootsample/SpringBootSample.java
@@ -0,0 +1,24 @@
+package springbootsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration 
+public class SpringBootSample {
+
+	
+	@RequestMapping("/")
+	public String home() {
+		return "Hello World : Successful with JAR";
+	}
+
+	
+    public static void main( String[] args )
+    {
+        SpringApplication.run(SpringBootSample.class, args);
+    }
+
+}
